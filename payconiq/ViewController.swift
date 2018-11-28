e0dd296e274fb2b85cb30c83119ec6d25011b7b0
asif13 <asifjunaid7@gmail.com>
2018-11-28 15:59:20 +0400

    Initial Commit

---
+++ payconiq/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  payconiq
+//
+//  Created by Asif Junaid on 11/28/18.
+//  Copyright © 2018 Asif Junaid. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
